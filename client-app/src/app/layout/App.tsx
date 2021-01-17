e046403448b4260005c3bdd3c0eff0d00666c7db
Denys Shevchuk <dshevchuk@virtido.com>
2021-01-17 18:43:57 +0200

    -Add MobX state holder for api ui -Add loading on ui

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,72 +1,92 @@
-import React, { useState, useEffect, Fragment } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect, Fragment, SyntheticEvent } from 'react';
 import { Container } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 import { IActivity } from '../models/activity';
 import NavBar from '../../features/navbar/NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
 
 const App = () => {
-    const [activities, setActivities] = useState<IActivity[]>([]);
-    const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
-    const [editMode, setEditMode] = useState(false);
+  const [activities, setActivities] = useState<IActivity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
+  const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
+  const [target, setTarget] = useState('');
 
-    const handleSelectActivity = (id: string) => {
-      setSelectedActivity(activities.filter(a => a.id === id)[0]);
-      setEditMode(false);
-    }
-
-    const handleOpenCreateForm = () => {
-      setSelectedActivity(null);
-      setEditMode(true);
-    }
-
-  const handleCreateActivity = (activity: IActivity) => {
-      setActivities([...activities, activity]);
-      setSelectedActivity(activity);
-      setEditMode(false);
-  }
-
-  const handleEditActivity = (activity: IActivity) => {
-    setActivities([...activities.filter(a => a.id !== activity.id), activity]);
-    setSelectedActivity(activity);
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.filter(a => a.id === id)[0]);
     setEditMode(false);
   }
 
-  const handleDeleteActivity = (id: string) => {
-    setActivities([...activities.filter(a => a.id !== id)])
+  const handleOpenCreateForm = () => {
+    setSelectedActivity(null);
+    setEditMode(true);
   }
 
-    useEffect(() => {
-      axios.get<IActivity[]>('http://localhost:5000/api/Activities')
-      .then((response) => {
-          let activities: IActivity[] = [];
-          response.data.forEach(activity => {
-            activity.date = activity.date.split('T')[0];
-            activities.push(activity);
-          })
-          setActivities(activities)
-      });
-    }, []);
+  const handleCreateActivity = (activity: IActivity) => {
+    setSubmitting(true);
+    agent.Activities.create(activity).then(() => {
+      setActivities([...activities, activity]);
+      setSelectedActivity(activity);
+      setEditMode(false);
+    }).then(() => setSubmitting(false))
+  }
 
-    return (
-      <Fragment>
-        <NavBar openCreateFrom={handleOpenCreateForm}/>
-        <Container style={{marginTop: '7em'}}>
-          <ActivityDashboard 
-            activities={activities} 
-            selectActivity={handleSelectActivity} 
-            selectedActivity={selectedActivity}
-            editMode={editMode}
-            setEditMode={setEditMode}
-            setSelectedActivity={setSelectedActivity}
-            createActivity={handleCreateActivity}
-            editActivity={handleEditActivity}
-            deleteActivity={handleDeleteActivity}
-          />
-        </Container>
-      </Fragment>
-    );
+  const handleEditActivity = (activity: IActivity) => {
+    setSubmitting(true);
+    agent.Activities.update(activity).then(() => {
+      setActivities([...activities.filter(a => a.id !== activity.id), activity]);
+      setSelectedActivity(activity);
+      setEditMode(false);
+    }).then(() => setSubmitting(false))
+  }
+
+  const handleDeleteActivity = (event: SyntheticEvent<HTMLButtonElement>, id: string) => {
+    setSubmitting(true);
+    setTarget(event.currentTarget.name);
+    agent.Activities.delete(id).then(() => {
+      setActivities([...activities.filter(a => a.id !== id)])
+    }).then(() => setSubmitting(false))
+  }
+
+  useEffect(() => {
+    agent.Activities.list()
+    .then((response) => {
+        let activities: IActivity[] = [];
+        response.forEach(activity => {
+          activity.date = activity.date.split('T')[0];
+          activities.push(activity);
+        })
+        setActivities(activities)
+    }).then(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <LoadingComponent content='Loading activities...' />
+  }
+
+  return (
+    <Fragment>
+      <NavBar openCreateFrom={handleOpenCreateForm}/>
+      <Container style={{marginTop: '7em'}}>
+        <ActivityDashboard 
+          activities={activities} 
+          selectActivity={handleSelectActivity} 
+          selectedActivity={selectedActivity}
+          editMode={editMode}
+          setEditMode={setEditMode}
+          setSelectedActivity={setSelectedActivity}
+          createActivity={handleCreateActivity}
+          editActivity={handleEditActivity}
+          deleteActivity={handleDeleteActivity}
+          submitting={submitting}
+          target={target}
+        />
+      </Container>
+    </Fragment>
+  );
 }
 
 export default App;
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,13 +8,15 @@ interface IProps {
     activity: IActivity;
     createActivity: (activity: IActivity) => void;
     editActivity: (activity: IActivity) => void;
+    submitting: boolean;
 }
 
 const ActivityForm: React.FC<IProps> = ({
     setEditMode, 
     activity: initialFormState,
     createActivity,
-    editActivity
+    editActivity,
+    submitting
 }) => {
 
     const initializeForm = () => {
@@ -56,52 +58,53 @@ const ActivityForm: React.FC<IProps> = ({
         <Segment clearing>
             <Form onSubmit={handleSubmit}>
                 <Form.Input 
-                onChange={handleInputChange} 
-                name='title' 
-                placeholder='Title' 
-                value={activity.title}
+                    onChange={handleInputChange} 
+                    name='title' 
+                    placeholder='Title' 
+                    value={activity.title}
                 />
                 <Form.TextArea 
-                onChange={handleInputChange} 
-                name='description' rows={2} 
-                placeholder='Description' 
-                value={activity.description}
+                    onChange={handleInputChange} 
+                    name='description' rows={2} 
+                    placeholder='Description' 
+                    value={activity.description}
                 />
                 <Form.Input 
-                onChange={handleInputChange} 
-                name='category' 
-                placeholder='Category' 
-                value={activity.category}
+                    onChange={handleInputChange} 
+                    name='category' 
+                    placeholder='Category' 
+                    value={activity.category}
                 />
                 <Form.Input 
-                onChange={handleInputChange} 
-                name='date' 
-                type='date' 
-                placeholder='Date' 
-                value={activity.date}
+                    onChange={handleInputChange} 
+                    name='date' 
+                    type='date' 
+                    placeholder='Date' 
+                    value={activity.date}
                 />
                 <Form.Input 
-                onChange={handleInputChange} 
-                name='city' 
-                placeholder='City' 
-                value={activity.city}
+                    onChange={handleInputChange} 
+                    name='city' 
+                    placeholder='City' 
+                    value={activity.city}
                 />
                 <Form.Input 
-                onChange={handleInputChange} 
-                name='venue' 
-                placeholder='Venue' 
-                value={activity.venue}
+                    onChange={handleInputChange} 
+                    name='venue' 
+                    placeholder='Venue' 
+                    value={activity.venue}
                 />
                 <Button 
-                floated='right' 
-                positive type='submit' 
-                content='Submit'
+                    loading={submitting} 
+                    floated='right' 
+                    positive type='submit' 
+                    content='Submit'
                 />
-                <Button 
-                onClick={() => setEditMode(false)} 
-                floated='right' 
-                type='submit' 
-                content='Cancel' 
+                <Button
+                    onClick={() => setEditMode(false)} 
+                    floated='right' 
+                    type='submit' 
+                    content='Cancel' 
                 />
             </Form>
         </Segment>
