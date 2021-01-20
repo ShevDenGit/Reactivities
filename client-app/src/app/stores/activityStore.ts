6b3803124170c0088f145643272f0548024f1948
Denys Shevchuk <dshevchuk@virtido.com>
2021-01-20 16:20:24 +0200

    add semantic-ui styling

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -11,8 +11,18 @@ class ActivityStore {
     @observable target = '';
 
     @computed get activitiesByDate () {
-        return Array.from(this.activityRegistry.values())
-        .slice().sort((a, b) => Date.parse(a.date) - Date.parse(b.date))
+        return this.groupActivitiesByDate(Array.from(this.activityRegistry.values()));
+    }
+
+    groupActivitiesByDate(activities: IActivity[]) {
+        const sortedActivities = activities.slice().sort(
+            (a, b) => Date.parse(a.date) - Date.parse(b.date)
+        )
+        return Object.entries(sortedActivities.reduce((activities, activity) => {
+            const date = activity.date;
+            activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+            return activities;
+        }, {} as {[key: string]: IActivity[]}));
     }
 
     @action loadActivities = async () => {
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,17 +1,20 @@
 import { observer } from 'mobx-react-lite'
 import React, { useContext, useEffect } from 'react'
-import { Link, RouteComponentProps } from 'react-router-dom'
-import { Button, Card, Image } from 'semantic-ui-react'
+import { RouteComponentProps } from 'react-router-dom'
+import { Grid } from 'semantic-ui-react'
 import LoadingComponent from '../../../app/layout/LoadingComponent'
 import ActivityStore from '../../../app/stores/activityStore'
+import ActivityDetailsChat from './ActivityDetailsChat'
+import ActivityDetailsHeader from './ActivityDetailsHeader'
+import ActivityDetailsInfo from './ActivityDetailsInfo'
+import ActivityDetailsSideBar from './ActivityDetailsSideBar'
 
 interface DetailParams {
     id: string;
 }
 
 const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({
-    match,
-    history
+    match
 }) => {
     const activityStore = useContext(ActivityStore);
     const { 
@@ -27,34 +30,16 @@ const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({
     if (loadingInitial || !activity) return <LoadingComponent content='Loading activity' />
 
     return (
-        <div>
-            <Card fluid>
-                <Image src={`/assets/categoryImages/${activity!.category}.jpg`} wrapped ui={false} />
-                <Card.Content>
-                    <Card.Header>{activity!.title}</Card.Header>
-                    <Card.Meta>
-                        <span>{activity!.date}</span>
-                    </Card.Meta>
-                    <Card.Description>
-                        {activity!.description}
-                    </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                    <Button.Group widths={2}>
-                        <Button 
-                        as={Link} to={`/manage/${activity.id}`}
-                        basic 
-                        color='blue' 
-                        content='Edit'/>
-                        <Button
-                         onClick={() => history.push('/activities')} 
-                         basic 
-                         color='grey' 
-                         content='Cancel' />
-                    </Button.Group>
-                </Card.Content>
-            </Card>
-        </div>
+        <Grid>
+            <Grid.Column width={10}>
+                <ActivityDetailsHeader activity={activity}/>
+                <ActivityDetailsInfo activity={activity}/>
+                <ActivityDetailsChat />
+            </Grid.Column>
+            <Grid.Column width={6}>
+                <ActivityDetailsSideBar />
+            </Grid.Column>
+        </Grid>
     )
 }
 
